a163f83da2ffb897483db69b622756c49c66f807
Amanda Haubert <amanda.pagliarin@aluno.feliz.ifrs.edu.br>
2025-08-24 19:16:04 -0300

    Create Registrations

--- config/routes.rb
+++ config/routes.rb
@@ -1,17 +1,19 @@
 Rails.application.routes.draw do
-  resource :session
+  # Signup
+  resource :registration, only: [ :new, :create ]
+  get "signup", to: "registrations#new"
+
+  # Login
+  resource :session, except: %i[ new ]
+  get "login", to: "sessions#new", as: :new_session
+
   resources :passwords, param: :token
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
-  # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
-  # Can be used by load balancers and uptime monitors to verify that the app is live.
-  get "up" => "rails/health#show", as: :rails_health_check
-
-  # Render dynamic PWA files from app/views/pwa/* (remember to link manifest in application.html.erb)
-  # get "manifest" => "rails/pwa#manifest", as: :pwa_manifest
-  # get "service-worker" => "rails/pwa#service_worker", as: :pwa_service_worker
-
-  # Defines the root path route ("/")
+  # Home Page
   root "pages#home"
-  get "dashboard", to: "pages#dashboard"
+
+  # Dashboard Page
+  get "dashboard", to: "pages#dashboard", as: :dashboard
+
+  get "up" => "rails/health#show"
 end
